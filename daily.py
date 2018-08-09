838762c7a0f0b4e3e056b762e9838d06fa638086
Vincent Cunningham <v.cunningham@flagrama.com>
2018-08-08 22:42:08 -0400

    Switch to JSON for configuration and option weighting

--- daily.py
+++ daily.py
@@ -5,111 +5,82 @@ import datetime
 import random
 import asyncio
 import logging
+import json
 from pathlib import Path
 
 import discord
 import git
 from numpy.random import choice
 
-REPO_URL = 'https://github.com/TestRunnerSRL/OoT-Randomizer.git'
-BRANCH = 'Dev'
-LOCAL_REPO = 'rando'
-CHANNEL_ID = '216764726988767232'
-ROM = os.path.join(os.getcwd(), 'rom', 'Legend of Zelda, The - Ocarina of Time (U) (V1.0).z64')
-WAD = os.path.join(os.getcwd(), 'rom', 'Legend of Zelda, The - Ocarina of Time (U) (V1.2) [Wii VC].wad')
-OUTPUT_DIR = os.path.join(os.getcwd(), 'dailies', str(datetime.date.today()))
+logger = None
 
-def update_rando():
+def update_rando(settings_json):
     repo = git.Repo
-
-    if not os.path.isdir(os.path.join(os.getcwd(), LOCAL_REPO)):
-        repo = git.Repo.clone_from(REPO_URL, LOCAL_REPO)
-        repo.git.checkout(BRANCH)
+    print('git stuff')
+    if not os.path.isdir(os.path.join(os.getcwd(), settings_json['config']['repo_local_name'])):
+        print('git clone new')
+        repo = git.Repo.clone_from(settings_json['config']['repo_url'], settings_json['config']['repo_local_name'])
+        repo.git.checkout(settings_json['config']['repo_branch'])
     else:
-        repo = git.Repo(os.path.join(os.getcwd(), LOCAL_REPO))
-        commits_behind = repo.iter_commits(BRANCH + '..origin/' + BRANCH)
+        print('git update')
+        repo = git.Repo(os.path.join(os.getcwd(), settings_json['config']['repo_local_name']))
+        repo.remotes.origin.fetch()
+        commits_behind = repo.iter_commits(settings_json['config']['repo_branch'] + '..origin/' + settings_json['config']['repo_branch'])
         count = sum(1 for c in commits_behind)
         if(count > 0):
-            repo = git.Repo(os.path.join(os.getcwd(), LOCAL_REPO))
-            repo.git.checkout(BRANCH)
+            repo = git.Repo(os.path.join(os.getcwd(), settings_json['config']['repo_local_name']))
+            repo.git.checkout(settings_json['config']['repo_branch'])
             repo.git.reset('--hard')
             repo.remotes.origin.pull()
             repo.git.execute(['git','apply', os.path.join(os.getcwd(), 'patches/0001-output-string.patch')])
 
-async def get_settings():
+async def get_settings(settings_json):
     # Settings by weight
-    settings = []
+    generator_settings = []
 
-    # Create output directory
-    if not os.path.isdir(OUTPUT_DIR):
-        if not os.path.isdir(os.path.join(os.getcwd(), 'dailies')):
-            os.mkdir(os.path.join(os.getcwd(), 'dailies'))
+    # Logic with multiple choices
+    for setting in settings_json['settings_weighted']:
+        if setting == 'other':
+            continue
+        choices = []
+        weights = []
+        argument = ''
+        value = ''
 
-    # Openess
-    openness_choices = [                    'closed',   'opendoor',     'openforest',   'open']
-    openness = choice(openness_choices, p=[ 0.25,       0.25,           0.25,           0.25])
-    if(openness == 'opendoor'):
-        settings.extend(['--open_door_of_time'])
-    elif(openness == 'openforest'):
-        settings.extend(['--open_forest'])
-    elif(openness == 'open'):
-        settings.extend(['--open_forest', '--open_door_of_time'])
+        if 'argument' in settings_json['settings_weighted'][setting]:
+            argument = settings_json['settings_weighted'][setting]['argument']
+        for i, option in enumerate(settings_json['settings_weighted'][setting]['options']):
+            choices.extend([settings_json['settings_weighted'][setting]['options'][i]['name']])
+            weights.extend([settings_json['settings_weighted'][setting]['options'][i]['weight']])
 
-    # Bridge Requirements
-    bridge_choices = [                  'medallions',   'vanilla',  'dungeons',     'open']
-    bridge = choice(bridge_choices, p=[ 0.30,           0.25,       0.20,           0.25])
-    if(bridge == 'vanilla'):
-        settings.extend(['--bridge', 'vanilla'])
-    elif(bridge == 'dungeons'):
-        settings.extend(['--bridge', 'dungeons'])
-    elif(bridge == 'open'):
-        settings.extend(['--bridge', 'open'])
+        result = choice(choices, p=weights)
+        if argument:
+            value = result
+        else:
+            value = ''
+        for i, option in enumerate(settings_json['settings_weighted'][setting]['options']):
+            if result == option['name']:
+                if 'argument' in option:
+                    argument = option['argument']
 
-    # Number of Trials
-    trial_chocies = list(range(7))
-    trials = choice(trial_chocies, p=[0.30, 0.05, 0.05, 0.20, 0.05, 0.05, 0.30])
-    settings.extend(['--trials', str(trials)])
-
-    # Tokensanity
-    tokensanity_choices = [                         'off',  'dungeons',     'all']
-    tokensanity = choice(tokensanity_choices, p=[   0.75,   0.15,           0.10])
-    if(tokensanity == 'dungeons'):
-        settings.extend(['--tokensanity', 'dungeons'])
-    elif(tokensanity == 'all'):
-        settings.extend(['--tokensanity', 'all'])
+        if argument:
+            generator_settings.extend(argument.split(' '))
+        if value:
+            generator_settings.extend([value])
 
     # Other Logic
-    if(random.random() < 0.5):
-        settings.extend(['--bombchus_in_logic'])
-    if(random.random() < 0.5):
-        settings.extend(['--all_reachable'])
-    if(random.random() < 0.15):
-        settings.extend(['--keysanity'])
-    if(random.random() < 0.30):
-        settings.extend(['--shuffle_gerudo_card'])
-    if(random.random() < 0.30):
-        settings.extend(['--shuffle_ocarinas'])
-    if(random.random() < 0.30):
-        settings.extend(['--shuffle_weird_egg'])
-    if(random.random() < 0.05):
-        settings.extend(['--ocarina_songs'])
-    if(random.random() < 0.30):
-        settings.extend(['--correct_chest_sizes'])
-    if(random.random() < 0.01):
-        settings.extend(['--ohko'])
-    if(random.random() < 0.25):
-        settings.extend(['--nodungeonitems'])
-    if(random.random() < 0.30):
-        settings.extend(['--progressive_bombchus'])
+    for i, setting in enumerate(settings_json['settings_weighted']['other']):
+        if(random.random() < settings_json['settings_weighted']['other'][i]['weight']):
+            generator_settings.extend(['--' + settings_json['settings_weighted']['other'][i]['name']])
 
-    return settings
+    return generator_settings
 
-async def create_daily(settings):
+async def create_daily(setting, settings_json):
 
     if(sys.platform == 'linux'):
-        base_settings = ['python3', os.path.join(os.getcwd(), 'rando', 'OoTRandomizer.py'), '--rom', ROM, '--output_dir', OUTPUT_DIR, '--hints', 'agony', '--no_escape_sequence', '--world_count', '1', '--player_num', '1']
+        base_settings = ['python3', os.path.join(os.getcwd(), 'rando', 'OoTRandomizer.py'), '--rom', os.path.join(os.getcwd(), 'rom', settings_json['config']['base_rom_name']), '--output_dir', os.path.join(settings_json['config']['output_directory'], str(datetime.date.today())), '--hints', 'agony', '--no_escape_sequence', '--world_count', '1', '--player_num', '1']
         f = open('output', 'w')
-        subprocess.call(base_settings + settings, stdout=f)
+        subprocess.call(base_settings + setting, stdout=f)
         f.close()
 
         strings = []
@@ -123,17 +94,19 @@ async def create_daily(settings):
         seed = strings[1].strip()
 
         rom_name = 'OoT_' + settings_string + '_' + seed
-        process = await asyncio.create_subprocess_exec('Compress/Compress', os.path.join(OUTPUT_DIR, rom_name + '.z64'), cwd='./rando', stdout=asyncio.subprocess.PIPE)
+        process = await asyncio.create_subprocess_exec('Compress/Compress', os.path.join(os.path.join(settings_json['config']['output_directory'], str(datetime.date.today())), rom_name + '.z64'), cwd='./rando', stdout=asyncio.subprocess.PIPE)
         await process.wait()
 
         # Create Wad
-        process = subprocess.call(['gzinject', '--cleanup', '-a', 'inject', '-w', WAD, '-i', 'NDYE', '-t', 'OoT Randomized', '-o', os.path.join(OUTPUT_DIR, rom_name + '.wad'), '--rom', os.path.join(OUTPUT_DIR, rom_name + '-comp.z64'), '--disable-controller-remappings'])
+        process = subprocess.call(['gzinject', '--cleanup', '-a', 'inject', '-w', os.path.join(os.getcwd(), 'rom', settings_json['config']['base_wad_name']), '-i', 'NDYE', '-t', 'OoT Randomized', '-o', os.path.join(os.path.join(settings_json['config']['output_directory'], str(datetime.date.today())), rom_name + '.wad'), '--rom', os.path.join(os.path.join(settings_json['config']['output_directory'], str(datetime.date.today())), rom_name + '-comp.z64'), '--disable-controller-remappings'])
 
     return rom_name, settings_string, seed
 
-async def upload_daily(rom_name):
-    process = await asyncio.create_subprocess_exec('python3', 'upload.py', rom_name, OUTPUT_DIR, stdout=asyncio.subprocess.PIPE)
+async def upload_daily(rom_name, settings_json):
+    process = await asyncio.create_subprocess_exec('python3', 'upload.py', rom_name, os.path.join(settings_json['config']['output_directory'], str(datetime.date.today())), stdout=asyncio.subprocess.PIPE)
     stdout, stderr = await process.communicate()
+    if stderr:
+        logger.error(stderr)
     link = stdout.decode().strip()
 
     return link
@@ -143,7 +116,10 @@ def main():
     logger = logging.getLogger('dailies')
     logger.setLevel(logging.INFO)
 
-    TOKEN = 'bot-token-here'
+    with open('settings.json') as data_file:
+        settings_json = json.loads(data_file.read())
+
+    TOKEN = settings_json['config']['discord_token']
 
     client = discord.Client()
 
@@ -158,22 +134,37 @@ def main():
     @client.event
     async def send_daily():
         await client.wait_until_ready()
-        channel = client.get_channel(CHANNEL_ID)
+        channel = client.get_channel(settings_json['config']['discord_channel_id'])
 
         while not client.is_closed:
+            # Create output directory
+            if not os.path.isdir(os.path.join(settings_json['config']['output_directory'], str(datetime.date.today()))):
+                if not os.path.isdir(os.path.join(os.getcwd(), 'dailies')):
+                    os.mkdir(os.path.join(os.getcwd(), 'dailies'))
+
+            # Create daily
+            update_rando(settings_json)
+            settings = await get_settings(settings_json)
+            rom_name, settings_string, seed = await create_daily(settings, settings_json)
+
+            version = open(os.path.join(os.getcwd(), settings_json['config']['repo_local_name'], 'version.py')).readline()
+            version = version.split('\'')
+
+            markdown = 'And now for ***DAILY SEED CHALLENGE ' + str(datetime.date.today()) + '***\n'
+            markdown = markdown + 'Version ' + version[1] + ' - Settings: ' + settings_string + ' - Seed: REDACTED'
+
+            message = await client.send_message(channel, content=markdown)
+
+            # Sleep until midnight (UTC)
             logger.info('sleeping for ' + str(how_many_seconds_until_midnight()) + ' seconds')
             await asyncio.sleep(how_many_seconds_until_midnight())
-
-            update_rando()
-            settings = await get_settings()
-            rom_name, settings_string, seed = await create_daily(settings)
-
-            message = await client.send_message(channel, content='Settings: ' + settings_string + '\n' + 'Seed: ' + seed)
             
-            link = await upload_daily(rom_name)
-            embed=discord.Embed(title="Download " + rom_name + " now!", url=link, description="Daily " + rom_name)
+            # Upload daily and add embed to message
+            link = await upload_daily(rom_name, settings_json)
+            markdown = markdown.replace('REDACTED', seed)
+            embed=discord.Embed(title="Download the daily challenge now!", url=link, description="Daily " + rom_name)
             embed.set_author(name="Daily Randomizer Challenge", url=link)
-            await client.edit_message(message, embed=embed)
+            await client.edit_message(message, markdown, embed=embed)
 
     client.loop.create_task(send_daily())
     client.run(TOKEN)
