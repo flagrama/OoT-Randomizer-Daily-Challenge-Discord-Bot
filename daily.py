0427d42f7f59ca1126212ec59473dd04147b29c0
Vincent Cunningham <v.cunningham@flagrama.com>
2018-08-10 18:27:27 -0400

    cleanup json usage

--- daily.py
+++ daily.py
@@ -15,21 +15,21 @@ from numpy.random import choice
 logger = None
 
 def update_rando(settings_json):
+    repo_name = settings_json['config']['repo_local_name']
+    repo_branch = settings_json['config']['repo_branch']
+
     repo = git.Repo
-    print('git stuff')
-    if not os.path.isdir(os.path.join(os.getcwd(), settings_json['config']['repo_local_name'])):
-        print('git clone new')
-        repo = git.Repo.clone_from(settings_json['config']['repo_url'], settings_json['config']['repo_local_name'])
-        repo.git.checkout(settings_json['config']['repo_branch'])
+    if not os.path.isdir(os.path.join(os.getcwd(), repo_name)):
+        repo = git.Repo.clone_from(settings_json['config']['repo_url'], repo_name)
+        repo.git.checkout(repo_branch)
     else:
-        print('git update')
-        repo = git.Repo(os.path.join(os.getcwd(), settings_json['config']['repo_local_name']))
+        repo = git.Repo(os.path.join(os.getcwd(), repo_name))
         repo.remotes.origin.fetch()
-        commits_behind = repo.iter_commits(settings_json['config']['repo_branch'] + '..origin/' + settings_json['config']['repo_branch'])
+        commits_behind = repo.iter_commits(repo_branch + '..origin/' + repo_branch)
         count = sum(1 for c in commits_behind)
         if(count > 0):
-            repo = git.Repo(os.path.join(os.getcwd(), settings_json['config']['repo_local_name']))
-            repo.git.checkout(settings_json['config']['repo_branch'])
+            repo = git.Repo(os.path.join(os.getcwd(), repo_name))
+            repo.git.checkout(repo_branch)
             repo.git.reset('--hard')
             repo.remotes.origin.pull()
             repo.git.execute(['git','apply', os.path.join(os.getcwd(), 'patches/0001-output-string.patch')])
@@ -47,18 +47,20 @@ async def get_settings(settings_json):
         argument = ''
         value = ''
 
-        if 'argument' in settings_json['settings_weighted'][setting]:
-            argument = settings_json['settings_weighted'][setting]['argument']
-        for i, option in enumerate(settings_json['settings_weighted'][setting]['options']):
-            choices.extend([settings_json['settings_weighted'][setting]['options'][i]['name']])
-            weights.extend([settings_json['settings_weighted'][setting]['options'][i]['weight']])
+        this_setting = settings_json['settings_weighted'][setting]
+
+        if 'argument' in this_setting:
+            argument = this_setting['argument']
+        for i, option in enumerate(this_setting['options']):
+            choices.extend([this_setting['options'][i]['name']])
+            weights.extend([this_setting['options'][i]['weight']])
 
         result = choice(choices, p=weights)
         if argument:
             value = result
         else:
             value = ''
-        for i, option in enumerate(settings_json['settings_weighted'][setting]['options']):
+        for i, option in enumerate(this_setting['options']):
             if result == option['name']:
                 if 'argument' in option:
                     argument = option['argument']
@@ -78,7 +80,7 @@ async def get_settings(settings_json):
 async def create_daily(setting, settings_json):
 
     if(sys.platform == 'linux'):
-        base_settings = ['python3', os.path.join(os.getcwd(), 'rando', 'OoTRandomizer.py'), '--rom', os.path.join(os.getcwd(), 'rom', settings_json['config']['base_rom_name']), '--output_dir', os.path.join(settings_json['config']['output_directory'], str(datetime.date.today())), '--hints', 'agony', '--no_escape_sequence', '--world_count', '1', '--player_num', '1']
+        base_settings = ['python3', os.path.join(os.getcwd(), 'rando', 'OoTRandomizer.py'), '--rom', os.path.join(os.getcwd(), 'rom', settings_json['config']['base_rom_name']), '--output_dir', os.path.join(settings_json['config']['output_directory'], str(datetime.date.today()))]
         f = open('output', 'w')
         subprocess.call(base_settings + setting, stdout=f)
         f.close()
@@ -134,19 +136,25 @@ def main():
     @client.event
     async def send_daily():
         await client.wait_until_ready()
-        channel = client.get_channel(settings_json['config']['discord_channel_id'])
 
         while not client.is_closed:
+            # Update JSON from file
+            with open('settings.json') as data_file:
+                settings_json = json.loads(data_file.read())
+
             # Create output directory
             if not os.path.isdir(os.path.join(settings_json['config']['output_directory'], str(datetime.date.today()))):
                 if not os.path.isdir(os.path.join(os.getcwd(), 'dailies')):
                     os.mkdir(os.path.join(os.getcwd(), 'dailies'))
+
+            channel = client.get_channel(settings_json['config']['discord_channel_id'])
 
             # Create daily
             update_rando(settings_json)
             settings = await get_settings(settings_json)
             rom_name, settings_string, seed = await create_daily(settings, settings_json)
 
+            # Create Discord message
             version = open(os.path.join(os.getcwd(), settings_json['config']['repo_local_name'], 'version.py')).readline()
             version = version.split('\'')
 
